7d0a1a20d32f46ae86bb528eb37b07033e7a55eb
Garech <118989052+Garech-mas@users.noreply.github.com>
2025-12-28 14:17:38 +0900

    バージョン更新

--- DoubleBPMGrid2/main.h
+++ DoubleBPMGrid2/main.h
@@ -8,7 +8,7 @@ struct LOG_HANDLE;
 
 // プラグイン情報定数
 #define PLUGIN_NAME L"BPMグリッド倍化"
-#define PLUGIN_VERSION L"v2.00a"
+#define PLUGIN_VERSION L"v2.1"
 #define TESTED_BETA L"beta26"
 #define TESTED_BETA_NO 2002600
 #define PLUGIN_TITLE PLUGIN_NAME " " PLUGIN_VERSION
